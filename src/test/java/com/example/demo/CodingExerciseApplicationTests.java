674e235818e4ab9bc7b3e26899471355fbbd448f
kdduwase <74629502+kdduwase@users.noreply.github.com>
2021-09-10 19:11:14 -0500

    coding exersice

---
+++ src/test/java/com/example/demo/CodingExerciseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+class CodingExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/example/demo/TestBreadController.java
@@ -0,0 +1,253 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.demo.Controller.BreadController;
+
+class TestBreadController {
+
+	@Autowired
+	BreadController breadController;
+	
+	@Autowired 
+	private WebApplicationContext ctx;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Before  
+	public void init() {  
+	    this.mockMvc = MockMvcBuilders.standaloneSetup(new BreadController()).build();
+	}  
+	 
+	String jsonString = "{\r\n"
+			+ "    \"wolfhound\": [\r\n"
+			+ "      \"irish\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"whippet\": [],\r\n"
+			+ "    \"weimaraner\": [],\r\n"
+			+ "    \"waterdog\": [\r\n"
+			+ "      \"spanish\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"vizsla\": [],\r\n"
+			+ "    \"terrier\": [\r\n"
+			+ "      \"american\",\r\n"
+			+ "      \"australian\",\r\n"
+			+ "      \"bedlington\",\r\n"
+			+ "      \"border\",\r\n"
+			+ "      \"dandie\",\r\n"
+			+ "      \"fox\",\r\n"
+			+ "      \"irish\",\r\n"
+			+ "      \"kerryblue\",\r\n"
+			+ "      \"lakeland\",\r\n"
+			+ "      \"norfolk\",\r\n"
+			+ "      \"norwich\",\r\n"
+			+ "      \"patterdale\",\r\n"
+			+ "      \"russell\",\r\n"
+			+ "      \"scottish\",\r\n"
+			+ "      \"sealyham\",\r\n"
+			+ "      \"silky\",\r\n"
+			+ "      \"tibetan\",\r\n"
+			+ "      \"toy\",\r\n"
+			+ "      \"westhighland\",\r\n"
+			+ "      \"wheaten\",\r\n"
+			+ "      \"yorkshire\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"stbernard\": [],\r\n"
+			+ "    \"springer\": [\r\n"
+			+ "      \"english\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"spaniel\": [\r\n"
+			+ "      \"blenheim\",\r\n"
+			+ "      \"brittany\",\r\n"
+			+ "      \"cocker\",\r\n"
+			+ "      \"irish\",\r\n"
+			+ "      \"japanese\",\r\n"
+			+ "      \"sussex\",\r\n"
+			+ "      \"welsh\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"shihtzu\": [],\r\n"
+			+ "    \"shiba\": [],\r\n"
+			+ "    \"sheepdog\": [\r\n"
+			+ "      \"english\",\r\n"
+			+ "      \"shetland\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"setter\": [\r\n"
+			+ "      \"english\",\r\n"
+			+ "      \"gordon\",\r\n"
+			+ "      \"irish\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"schnauzer\": [\r\n"
+			+ "      \"giant\",\r\n"
+			+ "      \"miniature\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"schipperke\": [],\r\n"
+			+ "    \"samoyed\": [],\r\n"
+			+ "    \"saluki\": [],\r\n"
+			+ "    \"rottweiler\": [],\r\n"
+			+ "    \"ridgeback\": [\r\n"
+			+ "      \"rhodesian\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"retriever\": [\r\n"
+			+ "      \"chesapeake\",\r\n"
+			+ "      \"curly\",\r\n"
+			+ "      \"flatcoated\",\r\n"
+			+ "      \"golden\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"redbone\": [],\r\n"
+			+ "    \"pyrenees\": [],\r\n"
+			+ "    \"puggle\": [],\r\n"
+			+ "    \"pug\": [],\r\n"
+			+ "    \"poodle\": [\r\n"
+			+ "      \"miniature\",\r\n"
+			+ "      \"standard\",\r\n"
+			+ "      \"toy\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"pomeranian\": [],\r\n"
+			+ "    \"pointer\": [\r\n"
+			+ "      \"german\",\r\n"
+			+ "      \"germanlonghair\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"pitbull\": [],\r\n"
+			+ "    \"pinscher\": [\r\n"
+			+ "      \"miniature\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"pembroke\": [],\r\n"
+			+ "    \"pekinese\": [],\r\n"
+			+ "    \"dhole\": [],\r\n"
+			+ "    \"deerhound\": [\r\n"
+			+ "      \"scottish\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"dane\": [\r\n"
+			+ "      \"great\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"dalmatian\": [],\r\n"
+			+ "    \"dachshund\": [],\r\n"
+			+ "    \"cotondetulear\": [],\r\n"
+			+ "    \"corgi\": [\r\n"
+			+ "      \"cardigan\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"coonhound\": [],\r\n"
+			+ "    \"collie\": [\r\n"
+			+ "      \"border\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"cockapoo\": [],\r\n"
+			+ "    \"clumber\": [],\r\n"
+			+ "    \"chow\": [],\r\n"
+			+ "    \"chihuahua\": [],\r\n"
+			+ "    \"cattledog\": [\r\n"
+			+ "      \"australian\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"cairn\": [],\r\n"
+			+ "    \"bullterrier\": [\r\n"
+			+ "      \"staffordshire\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"beagle\": [],\r\n"
+			+ "    \"basenji\": [],\r\n"
+			+ "    \"australian\": [\r\n"
+			+ "      \"shepherd\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"appenzeller\": [],\r\n"
+			+ "    \"akita\": [],\r\n"
+			+ "    \"airedale\": [],\r\n"
+			+ "    \"african\": [],\r\n"
+			+ "    \"affenpinscher\": [],\r\n"
+			+ "    \"bluetick\": [],\r\n"
+			+ "    \"borzoi\": [],\r\n"
+			+ "    \"bouvier\": [],\r\n"
+			+ "    \"boxer\": [],\r\n"
+			+ "    \"brabancon\": [],\r\n"
+			+ "    \"briard\": [],\r\n"
+			+ "    \"buhund\": [\r\n"
+			+ "      \"norwegian\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"bulldog\": [\r\n"
+			+ "      \"boston\",\r\n"
+			+ "      \"english\",\r\n"
+			+ "      \"french\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"dingo\": [],\r\n"
+			+ "    \"doberman\": [],\r\n"
+			+ "    \"elkhound\": [\r\n"
+			+ "      \"norwegian\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"entlebucher\": [],\r\n"
+			+ "    \"eskimo\": [],\r\n"
+			+ "    \"finnish\": [\r\n"
+			+ "      \"lapphund\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"frise\": [\r\n"
+			+ "      \"bichon\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"germanshepherd\": [],\r\n"
+			+ "    \"greyhound\": [\r\n"
+			+ "      \"italian\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"groenendael\": [],\r\n"
+			+ "    \"havanese\": [],\r\n"
+			+ "    \"hound\": [\r\n"
+			+ "      \"afghan\",\r\n"
+			+ "      \"basset\",\r\n"
+			+ "      \"blood\",\r\n"
+			+ "      \"english\",\r\n"
+			+ "      \"ibizan\",\r\n"
+			+ "      \"plott\",\r\n"
+			+ "      \"walker\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"husky\": [],\r\n"
+			+ "    \"keeshond\": [],\r\n"
+			+ "    \"kelpie\": [],\r\n"
+			+ "    \"komondor\": [],\r\n"
+			+ "    \"kuvasz\": [],\r\n"
+			+ "    \"labradoodle\": [],\r\n"
+			+ "    \"labrador\": [],\r\n"
+			+ "    \"leonberg\": [],\r\n"
+			+ "    \"lhasa\": [],\r\n"
+			+ "    \"malamute\": [],\r\n"
+			+ "    \"malinois\": [],\r\n"
+			+ "    \"maltese\": [],\r\n"
+			+ "    \"mastiff\": [\r\n"
+			+ "      \"bull\",\r\n"
+			+ "      \"english\",\r\n"
+			+ "      \"tibetan\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"mexicanhairless\": [],\r\n"
+			+ "    \"mix\": [],\r\n"
+			+ "    \"mountain\": [\r\n"
+			+ "      \"bernese\",\r\n"
+			+ "      \"swiss\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"newfoundland\": [],\r\n"
+			+ "    \"otterhound\": [],\r\n"
+			+ "    \"ovcharka\": [\r\n"
+			+ "      \"caucasian\"\r\n"
+			+ "    ],\r\n"
+			+ "    \"papillon\": []\r\n"
+			+ "}";
+	
+	
+	@Test
+	public void testBreadController() throws Exception {
+		 mockMvc.perform( MockMvcRequestBuilders
+			      .get("/breeds/list/all")
+			      .accept(MediaType.APPLICATION_JSON))
+			      .andExpect(status().isOk())
+			      .andExpect(MockMvcResultMatchers.jsonPath(jsonString).exists());
+			     
+	}
+	
+	
+
+}
